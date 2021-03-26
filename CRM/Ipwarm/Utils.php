f2feeac842476a3e63351ffe719eac587afb2066
Allen Shaw <allen@JoineryHQ.com>
2021-03-26 10:48:30 -0500

    Deprecated CRM_Ipwarm_Utils in favor of new CRM_Ipwarm_Limitmanager.

--- CRM/Ipwarm/Utils.php
+++ CRM/Ipwarm/Utils.php
@@ -4,44 +4,43 @@ use CRM_Ipwarm_ExtensionUtil as E;
 
 /**
  * Utility methods for ipwarm extension.
+ * 
+ * DEPRECATED: Please use comparable methods in CRM_Ipwarm_Limitmanager() object.
  *
  * @author as
  */
 class CRM_Ipwarm_Utils {
 
+  /**
+   * DEPRECATED: Please use comparable methods in CRM_Ipwarm_Limitmanager() object.
+   */
   public static function getWarmupSchedule() {
-    return [
-      // Age-in-days => ['hourly' => hourl-limit, 'daily' => daily-limit]
-      0 => ['hourly' => 20, 'daily' => 50],
-      1 => ['hourly' => 28, 'daily' => 100],
-      2 => ['hourly' => 39, 'daily' => 500],
-      3 => ['hourly' => 55, 'daily' => 1000],
-      4 => ['hourly' => 77, 'daily' => 5000],
-      5 => ['hourly' => 108, 'daily' => 10000],
-      6 => ['hourly' => 151, 'daily' => 20000],
-      7 => ['hourly' => 211, 'daily' => 40000],
-      8 => ['hourly' => 295, 'daily' => 70000],
-      9 => ['hourly' => 413, 'daily' => 100000],
-      10 => ['hourly' => 579, 'daily' => 150000],
-      11 => ['hourly' => 810, 'daily' => 250000],
-      12 => ['hourly' => 1000, 'daily' => 400000],
-      13 => ['hourly' => 1587, 'daily' => 600000],
-      14 => ['hourly' => 2222, 'daily' => 1000000],
-      15 => ['hourly' => 3111, 'daily' => 2000000],
-      16 => ['hourly' => 4356, 'daily' => 4000000],
-    ];
+    $limitManager = new CRM_Ipwarm_Limitmanager();
+    return call_user_func([$limitManager, __FUNCTION__]);
   }
 
+  /**
+   * DEPRECATED: Please use comparable methods in CRM_Ipwarm_Limitmanager() object.
+   */
   public static function getDailyUsage() {
-    return 700;
+    $limitManager = new CRM_Ipwarm_Limitmanager();
+    return call_user_func([$limitManager, __FUNCTION__]);
   }
 
+  /**
+   * DEPRECATED: Please use comparable methods in CRM_Ipwarm_Limitmanager() object.
+   */
   public static function getHourlyUsage() {
-    return 10;
+    $limitManager = new CRM_Ipwarm_Limitmanager();
+    return call_user_func([$limitManager, __FUNCTION__]);
   }
 
+  /**
+   * DEPRECATED: Please use comparable methods in CRM_Ipwarm_Limitmanager() object.
+   */
   public static function getCurrentLevel() {
-    return 3;
+    $limitManager = new CRM_Ipwarm_Limitmanager();
+    return call_user_func([$limitManager, __FUNCTION__]);
   }
 
 }
--- ipwarm.php
+++ ipwarm.php
@@ -14,7 +14,7 @@ function ipwarm_civicrm_config(&$config) {
   // Use a static var to ensure we don't add this listener twice, which can happen
   // when running the api with `cv`.
   if (!isset(\Civi::$statics[__FUNCTION__]) || !\Civi::$statics[__FUNCTION__]) {
-  Civi::dispatcher()->addListener(\Civi\API\Events::RESPOND, ['CRM_Ipwarm_APIWrapper', 'RESPOND'], -100);
+    Civi::dispatcher()->addListener(\Civi\API\Events::PREPARE, ['CRM_Ipwarm_APIWrapper', 'PREPARE'], -100);
     \Civi::$statics[__FUNCTION__] = true;
   }
   _ipwarm_civix_civicrm_config($config);
@@ -165,27 +165,14 @@ function ipwarm_civicrm_themes(&$themes) {
  *
  * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_navigationMenu
  */
-function ipwarm_civicrm_navigationMenu(&$menu) {
-  $pages = array(
-    'admin_page' => array(
-      'label' => E::ts('IP Warming Summary'),
-      'name' => 'IP Warming Summary',
-      'url' => 'civicrm/admin/ipwarm/summary?reset=1',
-      'parent' => array('Administer', 'CiviMail'),
-      'permission' => 'access CiviCRM',
-    ),
-  );
-
-  foreach ($pages as $page) {
-    // Check that our item doesn't already exist.
-    $menu_item_properties = array('url' => $page['url']);
-    $existing_menu_items = array();
-    CRM_Core_BAO_Navigation::retrieve($menu_item_properties, $existing_menu_items);
-    if (empty($existing_menu_items)) {
-      // Now we're sure it doesn't exist; add it to the menu.
-      $menuPath = implode('/', $page['parent']);
-      unset($page['parent']);
-      _ipwarm_civix_insert_navigation_menu($menu, $menuPath, $page);
-    }
-  }
-}
+//function ipwarm_civicrm_navigationMenu(&$menu) {
+//  _ipwarm_civix_insert_navigation_menu($menu, 'Mailings', array(
+//    'label' => E::ts('New subliminal message'),
+//    'name' => 'mailing_subliminal_message',
+//    'url' => 'civicrm/mailing/subliminal',
+//    'permission' => 'access CiviMail',
+//    'operator' => 'OR',
+//    'separator' => 0,
+//  ));
+//  _ipwarm_civix_navigationMenu($menu);
+//}
