9befb6f8b29604dda80f9a77019404c60d7e598e
Lucky <lucky091588@gmail.com>
2021-03-26 22:53:37 +0800

    Change variable name to noun

--- CRM/Ipwarm/Page/Summary.php
+++ CRM/Ipwarm/Page/Summary.php
@@ -6,12 +6,12 @@ class CRM_Ipwarm_Page_Summary extends CRM_Core_Page {
   public function run() {
     CRM_Utils_System::setTitle(E::ts('IP Warming Summary'));
 
-    $getSummaryList = CRM_Ipwarm_Utils::getWarmupSchedule();
-    $getCurrentLevel = CRM_Ipwarm_Utils::getCurrentLevel();
+    $summaryList = CRM_Ipwarm_Utils::getWarmupSchedule();
+    $currentLevel = CRM_Ipwarm_Utils::getCurrentLevel();
 
-    $getSummaryList[$getCurrentLevel]['class'] = 'crm-row-selected';
+    $summaryList[$currentLevel]['class'] = 'crm-row-selected';
 
-    $this->assign('summaryList', $getSummaryList);
+    $this->assign('summaryList', $summaryList);
 
     parent::run();
   }
